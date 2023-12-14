e0fe03d9078f6e3b635029a30ee0b55b5314736b
bardini <roberta.bardini@polito.it>
2023-12-14 13:34:56 +0000

    updating env for exp4

--- source/experiment4/env/PalacellEnv.py
+++ source/experiment4/env/PalacellEnv.py
@@ -76,138 +76,106 @@ class PalacellEnv():
     self.cell_increments = []
     self.epoch_cell_increments = []
 
-    if mode=='circles':
-      work1, work2 = Pipe(True)
-      prolif_env = PalacellEnv(iters=self.iters, configuration_file='circle_prolif_'+str(self.iters)+'_'+str(self.lr)+'_'+str(self.gamma)+'.xml',
-                              output_file='chem_'+str(self.iters)+'_'+str(self.lr)+'_'+str(self.gamma)+'-',
-                              output_dir='experiment2/inner/new2_palacell_circle_prolif_out_'+str(self.iters), max_iterations=2500,
-                              lr=self.lr, gamma=self.gamma, target=self.target, mode='circle_prolif')
-      prolif_env.epochs = self.epochs
-      suf = base_outfolder+"/experiment2/inner/new2_palacell_circle_prolif_out_"+str(self.iters)+'_'+str(lr)+'_'+str(gamma)
-      if self.preload_model_weights:
-        prolif_env.preload_model_weights = suf+"/model_at_epoch_"+str(starting_epoch)+".h5"
-      if self.preload_data_to_save:
-        prolif_env.preload_data_to_save = suf+"/data_to_save_at_epoch_"+str(starting_epoch)
-      if self.preload_performance:
-        prolif_env.preload_performance = suf+"/performance_at_epoch_"+str(starting_epoch)
-      self.prolif_env = prolif_env
-      prolif_train = ppt.ProlifTrain(self.prolif_env, self.lr, self.gamma)
-      prolif_proc = Process(target=prolif_train.train, args=[5, False, starting_epoch, work1])
-      prolif_proc.start()
-      self.prolif_train = prolif_train
-      self.prolif_proc = prolif_proc
-      self.pipe = work2
-
   def reset(self): #todo!
     cwd = os.getcwd()
     os.chdir(base_palacell_folder)
-    if self.mode == 'circles':
-      observation = np.asarray(Image.new("RGB", (self.width,self.height), (0,0,0))).copy()
-      observation = vki.add_target(observation, [self.target[0]*3/4, self.target[1]*3/4], self.target[2]*3/4).reshape((1,self.width,self.height,3)).copy()
-    elif self.mode == 'circle_prolif':
-      self.last_cell_num = 0
-      self.epoch_compr = []
-      self.epoch_cell_increments = []
-      self.iteration_num = 0
-      try:
-        process = Popen(['./palaCell',self.configuration], stdout=DEVNULL)
-        process.wait()
-      except Exception as e:
-        os.chdir(cwd)
-        raise Exception(e)
-      image = vki.create_pil_image("output/"+self.output_file+"_final_cell")
-      observation = vki.pil_to_array(image).reshape((1,self.width,self.height,3))
+    
+    observation = np.asarray(Image.new("RGB", (self.width,self.height), (0,0,0))).copy()
+    observation = vki.add_target(observation, [self.target[0]*3/4, self.target[1]*3/4], self.target[2]*3/4).reshape((1,self.width,self.height,3)).copy()
+    self.last_cell_num = 0
+    self.epoch_compr = []
+    self.epoch_cell_increments = []
+    self.iteration_num = 0
+    
+    image = vki.create_pil_image("output/"+self.output_file+"_final_cell")
+    observation = vki.pil_to_array(image).reshape((1,self.width,self.height,3))
     os.chdir(cwd)
+    
     return observation
 
   def _get_info(self):
-    st = ""
-    if self.mode == 'circle_prolif':
-      st = "last cell num: "+str(self.last_cell_num)+", best least iterations num: "+str(self.least_iterations)+", best cell num: "+str(self.best_cell_num)
-    elif self.mode=='circles':
-      st = "last inside-outside: "+str(self.last_inside_outside)+", best last inside-outside: "+str(self.best_inside_outside)
-    st = st+" elapsed time: "+str(time.time()-self.start_time)+" iteration: "+str(self.iteration_num)+" iters: "+str(self.iters)
-    return st
+    lc, io = "", ""
+    
+    lc = "last cell num: "+str(self.last_cell_num)+", best least iterations num: "+str(self.least_iterations)+", best cell num: "+str(self.best_cell_num)
+    io = "last inside-outside: "+str(self.last_inside_outside)+", best last inside-outside: "+str(self.best_inside_outside)
+    lc = lc+" elapsed time: "+str(time.time()-self.start_time)+" iteration: "+str(self.iteration_num)+" iters: "+str(self.iters)
+    io = io+" elapsed time: "+str(time.time()-self.start_time)+" iteration: "+str(self.iteration_num)+" iters: "+str(self.iters)
+    return st, io
 
   def load_data_to_save(self, data):
-    if self.mode == 'circle_prolif':
-      self.cell_numbers = data['cell_numbers']
-      self.cell_increments = data['cell_increments']
-      self.compr_history = data['compr_history']
-    elif self.mode=='circles':
-      self.inside_outside = data['inside_outside']
-      self.circle_actions = data['circle_actions']
+    
+    self.cell_numbers = data['cell_numbers']
+    self.cell_increments = data['cell_increments']
+    self.compr_history = data['compr_history']
+    
+    self.inside_outside = data['inside_outside']
+    self.circle_actions = data['circle_actions']
 
   def data_to_save(self):
     out = {}
-    if self.mode == 'circle_prolif':
-      out['cell_numbers'] = self.cell_numbers
-      out['cell_increments'] = self.cell_increments
-      out['compr_history'] = self.compr_history
-    elif self.mode == 'circles':
-      out['inside_outside'] = self.inside_outside
-      out['circle_actions'] = self.circle_actions
+    
+    out['cell_numbers'] = self.cell_numbers
+    out['cell_increments'] = self.cell_increments
+    out['compr_history'] = self.compr_history
+    
+    out['inside_outside'] = self.inside_outside
+    out['circle_actions'] = self.circle_actions
+    
     return out
 
   def step(self, action):
+    
     cwd = os.getcwd()
     os.chdir(base_palacell_folder)
 
-    if self.mode == 'circle_prolif':
-      self.epoch_compr.append(action)
-      cont_action = float(action[1])
-      cont_action = math.floor(cont_action*(10)+0.5)/(10**3)
-      action[1] = str(cont_action)
-      self.configure(self.configuration, self.iters, action[0], action[1], export_step=self.iters, initialPath = "output/"+self.output_file+"_final_cell.vtp",
-        finalPath = self.output_file)
-      try:
-        process = Popen(['./palaCell',self.configuration], stdout=DEVNULL)
-        process.wait()
-      except Exception as e:
-        os.chdir(cwd)
-        raise Exception(e)
+    self.epoch_compr.append(action)
+    cont_action = float(action[1])
+    cont_action = math.floor(cont_action*(10)+0.5)/(10**3)
+    action[1] = str(cont_action)
 
+    self.configure(self.configuration, self.iters, action[0], action[1], export_step=self.iters, initialPath = "output/"+self.output_file+"_final_cell.vtp",
+        finalPath = self.output_file)
+
+    try:
+      process = Popen(['./palaCell',self.configuration], stdout=DEVNULL)
+      process.wait()
+    except Exception as e:
       os.chdir(cwd)
-      observation = img_to_array(self.render()).reshape((1,self.width,self.height,3))
-      cwd = os.getcwd()
-      os.chdir(base_palacell_folder)
-      cell_num = vki.read_cell_num("output/"+self.output_file+"_final_cell")
-      if cell_num>self.best_cell_num:
+      raise Exception(e)
+
+    os.chdir(cwd)
+    observation = img_to_array(self.render()).reshape((1,self.width,self.height,3))
+    cwd = os.getcwd()
+    os.chdir(base_palacell_folder)
+    cell_num = vki.read_cell_num("output/"+self.output_file+"_final_cell")
+    
+    if cell_num>self.best_cell_num:
         self.best_cell_num = cell_num
         self.performance_updated = True
-      reward = cell_num - self.last_cell_num
-      self.epoch_cell_increments.append(reward)
-      self.last_cell_num = cell_num
-      self.iteration_num += self.iters
+    
+    increment = cell_num - self.last_cell_num
+    
+    self.epoch_cell_increments.append(increment)
+    self.last_cell_num = cell_num
+    self.iteration_num += self.iters
 
-      if cell_num>=300 or self.iteration_num>=self.max_iterations:
-        done = True
-      else:
-        done = False
-      if done:
-        self.compr_history.append(self.epoch_compr)
-        print()
-        print(self.epoch_compr)
-        print()
-        if self.iteration_num < self.least_iterations:
-          self.least_iterations = self.iteration_num 
-        self.cell_numbers.append(cell_num)
-        self.cell_increments.append(self.epoch_cell_increments)
-    elif self.mode == 'circles':
-      print(action)
-      self.circle_actions.append(action)
-      self.configure(self.configuration,0,finalPath = 'chem_'+str(self.iters)+'_'+str(self.lr)+'_'+str(self.gamma)+'-',initial_position=np.array(action))
-      
-      '''
-      wait for training process of prolif
-      '''
-      self.pipe.send(self.configuration)
-      out_path = self.pipe.recv()
-
+    if cell_num>=300 or self.iteration_num>=self.max_iterations:
       done = True
-      outside, inside = vki.count_target_points(out_path, [self.target[0], self.target[1]], self.target[2])
-      cell_num = vki.read_cell_num(out_path)
-      reward = (inside-outside)/cell_num
+    else:
+      done = False
+    if done:
+      self.compr_history.append(self.epoch_compr)
+      print()
+      print(self.epoch_compr)
+      print()
+      if self.iteration_num < self.least_iterations:
+        self.least_iterations = self.iteration_num 
+      self.cell_numbers.append(cell_num)
+      self.cell_increments.append(self.epoch_cell_increments)
+      
+      outside, inside = vki.count_target_points("output/"+self.output_file+"_final_cell", [self.target[0], self.target[1]], self.target[2])
+      cell_num = vki.read_cell_num("output/"+self.output_file+"_final_cell")
+      normFraction = (inside-outside)/cell_num
       self.inside_outside.append((inside/cell_num,outside/cell_num))
       diff = self.best_inside_outside[0]-self.best_inside_outside[1]
       if diff < (inside-outside)/cell_num or (diff == (inside-outside)/cell_num and self.best_inside_outside[0]<inside/cell_num):
@@ -215,11 +183,12 @@ class PalacellEnv():
         self.performance_updated = True
       self.last_inside_outside = (inside/cell_num, outside/cell_num)
       print("inside/outside: "+str(inside/cell_num)+"/"+str(outside/cell_num))
-      image = vki.create_decentered_pil_image(out_path).resize([self.width, self.height])
+      image = vki.create_decentered_pil_image("output/"+self.output_file+"_final_cell").resize([self.width, self.height])
       observation = vki.pil_to_array(image)
       observation = vki.add_target(observation, [self.target[0]*3/4, self.target[1]*3/4], self.target[2]*3/4).reshape((1,self.width,self.height,3)).copy()
-    os.chdir(cwd)
-    return observation, reward, done, None
+      os.chdir(cwd)
+    
+    return observation, cell_num, normFraction, done, None
 
   def render(self):
     cwd = os.getcwd()
@@ -272,6 +241,14 @@ class PalacellEnv():
 
   def get_performance(self):
     return self.performance_indexes
+  
+  def getNormFrac(self):
+    return self.inside_outside
+  
+  def getCellNum(self):
+    return self.last_cell_num
+  
+
 
   def configure(self, filePath, num_iter=5, axis='X', compr_force=0.0, export_step='0', init='0', initialPath = ' ', initialWallPath = ' ', initial_position = [200,200], finalPath = 'chem_1-'):
     root = ET.Element('parameters')
